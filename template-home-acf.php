738089fadf375879d3e3c90ff3d0be8fe3b9e127
Pradeep Verma <pradeepofficial8795@gmail.com>
2026-09-08 21:59:54 +0530

    Remove hardcoded hero section from template; dynamic rendering via [ci360_home_hero] shortcode only

--- template-home-acf.php
+++ template-home-acf.php
@@ -10,10 +10,7 @@ get_header();
 
 <main id="primary" class="site-main bg-slate-950 text-white min-h-screen">
     <?php 
-    // Render Dynamic ACF Hero Section
-    get_template_part( 'template-parts/home-hero-acf' ); 
-
-    // Elementor Content Area (Required by Elementor Editor)
+    // Pure Elementor & WordPress Content (Use [ci360_home_hero] shortcode in Elementor)
     while ( have_posts() ) :
         the_post();
         the_content();
@@ -23,4 +20,5 @@ get_header();
 
 <?php
 get_footer();
+
 
